70f7a11ef48a968bb6ba32bf178ef80b34b21e32
intelimaster <intelimaster0314@gmail.com>
2025-01-02 11:34:24 +0530

    fix: comments and changes

--- programs/gamma/src/instructions/rebalance.rs
+++ programs/gamma/src/instructions/rebalance.rs
@@ -1,4 +1,5 @@
 use crate::{
+    error::GammaError,
     fees::FEE_RATE_DENOMINATOR_VALUE,
     states::{PoolState, POOL_KAMINO_DEPOSITS_SEED},
 };
@@ -49,14 +50,17 @@ pub struct Rebalance<'info> {
     pub token_mint: Box<InterfaceAccount<'info, Mint>>,
 
     // Kamino deposit and withdraw related accounts.
+    /// CHECK: The account address is checked in the cpi.
     #[account(mut)]
-    pub kamino_reserve: AccountInfo<'info>,
+    pub kamino_reserve: UncheckedAccount<'info>,
 
+    /// CHECK: The account address is checked in the cpi.
     #[account(mut)]
-    pub kamino_lending_market: AccountInfo<'info>,
+    pub kamino_lending_market: UncheckedAccount<'info>,
 
+    /// CHECK: The account address is checked in the cpi.
     #[account()]
-    pub lending_market_authority: AccountInfo<'info>,
+    pub lending_market_authority: UncheckedAccount<'info>,
 
     #[account(mut)]
     pub reserve_liquidity_supply: Box<InterfaceAccount<'info, TokenAccount>>,
@@ -101,7 +105,7 @@ pub fn rebalance_kamino<'c, 'info>(
     let deposit_withdraw_amounts = get_deposit_withdraw_amounts(
         ctx.accounts.pool_state.clone(),
         ctx.accounts.token_vault.clone(),
-        ctx.accounts.kamino_reserve.clone(),
+        ctx.accounts.kamino_reserve.to_account_info(),
         ctx.accounts.gamma_pool_destination_collateral.clone(),
     )?;
     if deposit_withdraw_amounts.should_do_nothing {
@@ -113,21 +117,102 @@ pub fn rebalance_kamino<'c, 'info>(
         &[deposit_withdraw_amounts.pool_state_auth_bump],
     ]];
 
+    let amount_in_kamino_reserve_before = ctx.accounts.reserve_liquidity_supply.amount;
     if deposit_withdraw_amounts.should_deposit {
         deposit_in_kamino(
-            ctx,
+            &ctx,
             deposit_withdraw_amounts.amount_to_deposit_withdraw,
             signer_seeds,
         )?;
     } else {
         withdraw_from_kamino(
-            ctx,
+            &ctx,
             deposit_withdraw_amounts.amount_to_deposit_withdraw,
             signer_seeds,
         )?;
     }
 
+    let mut pool_state = ctx.accounts.pool_state.load_mut()?;
+
+    ctx.accounts.token_vault.reload()?;
+    let amount_in_pool_token_account_after = ctx.accounts.token_vault.amount;
+    let amount_in_kamino_after_rebalance = get_amounts_in_kamino_after_rebalance(
+        ctx.accounts.kamino_reserve.to_account_info(),
+        &mut ctx.accounts.gamma_pool_destination_collateral,
+    )?;
+
+    // This is the actual amount that was deposited in kamino.
+    // Stored here for easy access of how much was deposited at time of rebalance.
+    if !deposit_withdraw_amounts.is_withdrawing_profit {
+        ctx.accounts.reserve_liquidity_supply.reload()?;
+        let amount_in_kamino_reserve_after = ctx.accounts.reserve_liquidity_supply.amount;
+
+        let amount_changed = if deposit_withdraw_amounts.should_deposit {
+            amount_in_kamino_reserve_after
+                .checked_sub(amount_in_kamino_reserve_before)
+                .ok_or(GammaError::MathOverflow)?
+        } else {
+            amount_in_kamino_reserve_before
+                .checked_sub(amount_in_kamino_reserve_after)
+                .ok_or(GammaError::MathOverflow)?
+        };
+
+        if deposit_withdraw_amounts.is_token_0 {
+            if deposit_withdraw_amounts.should_deposit {
+                pool_state.token_0_amount_in_kamino = pool_state
+                    .token_0_amount_in_kamino
+                    .checked_add(amount_changed)
+                    .ok_or(GammaError::MathOverflow)?;
+            } else {
+                pool_state.token_0_amount_in_kamino = pool_state
+                    .token_0_amount_in_kamino
+                    .checked_sub(amount_changed)
+                    .ok_or(GammaError::MathOverflow)?;
+            }
+        } else {
+            if deposit_withdraw_amounts.should_deposit {
+                pool_state.token_1_amount_in_kamino = pool_state
+                    .token_1_amount_in_kamino
+                    .checked_add(amount_changed)
+                    .ok_or(GammaError::MathOverflow)?;
+            } else {
+                pool_state.token_1_amount_in_kamino = pool_state
+                    .token_1_amount_in_kamino
+                    .checked_sub(amount_changed)
+                    .ok_or(GammaError::MathOverflow)?;
+            }
+        }
+    }
+
+    // In any case, we want to make sure that the token_0_vault_amount and token_1_vault_amount are updated.
+    match deposit_withdraw_amounts.is_token_0 {
+        true => {
+            pool_state.token_0_vault_amount = amount_in_pool_token_account_after
+                .checked_add(amount_in_kamino_after_rebalance)
+                .ok_or(GammaError::MathOverflow)?;
+        }
+        false => {
+            pool_state.token_1_vault_amount = amount_in_pool_token_account_after
+                .checked_add(amount_in_kamino_after_rebalance)
+                .ok_or(GammaError::MathOverflow)?;
+        }
+    }
+
     Ok(())
+}
+
+fn get_amounts_in_kamino_after_rebalance<'info>(
+    kamino_reserve: AccountInfo<'info>,
+    gamma_pool_destination_collateral: &mut Box<InterfaceAccount<'info, TokenAccount>>,
+) -> Result<u64> {
+    gamma_pool_destination_collateral.reload()?;
+
+    let reserve: kamino_cpi::state::Reserve = load_account(&kamino_reserve)?;
+    let collateral_amount = gamma_pool_destination_collateral.amount;
+
+    let amount_deposited = reserve.redeem_collateral_expected(collateral_amount)?;
+
+    Ok(amount_deposited)
 }
 
 struct DepositWithdrawAmountResult {
@@ -136,6 +221,8 @@ struct DepositWithdrawAmountResult {
     should_do_nothing: bool,
     // it is amount to deposit or withdraw
     amount_to_deposit_withdraw: u64,
+    is_token_0: bool,
+    is_withdrawing_profit: bool,
 }
 
 fn get_deposit_withdraw_amounts<'c, 'info>(
@@ -151,7 +238,13 @@ fn get_deposit_withdraw_amounts<'c, 'info>(
 
     let collateral_amount = gamma_pool_destination_collateral.amount;
 
-    let amount_deposited = reserve.redeem_collateral_expected(collateral_amount)?;
+    let amount_in_kamino = reserve.redeem_collateral_expected(collateral_amount)?;
+
+    let amount_deposited = if is_token_0 {
+        pool_state.token_0_amount_in_kamino
+    } else {
+        pool_state.token_1_amount_in_kamino
+    };
 
     let max_deposit_allowed_rate = if is_token_0 {
         pool_state.max_shared_token0
@@ -164,25 +257,51 @@ fn get_deposit_withdraw_amounts<'c, 'info>(
     } else {
         pool_state.token_1_vault_amount
     };
-    let max_deposit_allowed =
-        amount_in_pool_vault * FEE_RATE_DENOMINATOR_VALUE / max_deposit_allowed_rate;
+    let max_deposit_allowed = amount_in_pool_vault
+        .checked_mul(FEE_RATE_DENOMINATOR_VALUE)
+        .ok_or(GammaError::MathOverflow)?
+        .checked_div(max_deposit_allowed_rate)
+        .ok_or(GammaError::MathOverflow)?;
 
     msg!("max_deposit_allowed: {}", max_deposit_allowed);
-    msg!("amount_deposited: {}", amount_deposited);
+    msg!("amount_in_kamino: {}", amount_in_kamino);
     msg!("amount_in_pool_vault: {}", amount_in_pool_vault);
     msg!("max_deposit_allowed_rate: {}", max_deposit_allowed_rate);
     msg!("collateral_amount: {}", collateral_amount);
     msg!("is_token_0: {}", is_token_0);
 
+    let mut is_withdrawing_profit = false;
+
+    let amount_to_deposit_withdraw = if max_deposit_allowed < amount_deposited {
+        // Deposit the difference between the max deposit allowed and the amount deposited.
+        max_deposit_allowed
+            .checked_sub(amount_deposited)
+            .ok_or(GammaError::MathOverflow)?
+    } else if max_deposit_allowed == amount_deposited {
+        is_withdrawing_profit = true;
+        // If this is the case we still want to withdraw the profit, if any,
+        amount_in_kamino
+            .checked_sub(amount_deposited)
+            .ok_or(GammaError::MathOverflow)?
+    } else {
+        // Withdraw the difference between the max deposit allowed and the amount deposited.
+        // We do a min here as in the worst case the amount in kamino is less than the amount deposited i.e we incurred loss on our deposits.
+        std::cmp::min(
+            amount_deposited
+                .checked_sub(max_deposit_allowed)
+                .ok_or(GammaError::MathOverflow)?,
+            amount_in_kamino,
+        )
+    };
+
     Ok(DepositWithdrawAmountResult {
         pool_state_auth_bump: pool_state.auth_bump,
-        should_do_nothing: max_deposit_allowed == amount_deposited,
+        // We don't need to do anything if we have deposited the max we wanted to deposit, and the amount in kamino is less than the amount deposited i.e there is no profits on the amount we put in kamino.
+        should_do_nothing: amount_to_deposit_withdraw == 0,
         should_deposit: max_deposit_allowed < amount_deposited,
-        amount_to_deposit_withdraw: if max_deposit_allowed < amount_deposited {
-            amount_deposited.checked_sub(max_deposit_allowed).unwrap()
-        } else {
-            max_deposit_allowed.checked_sub(amount_deposited).unwrap()
-        },
+        amount_to_deposit_withdraw,
+        is_token_0,
+        is_withdrawing_profit,
     })
 }
 
@@ -200,7 +319,7 @@ pub fn load_account<T: BorshDeserialize>(account_info: &AccountInfo) -> Result<T
 }
 
 pub fn deposit_in_kamino<'c, 'info>(
-    ctx: Context<'_, '_, 'c, 'info, Rebalance<'info>>,
+    ctx: &Context<'_, '_, 'c, 'info, Rebalance<'info>>,
     amount: u64,
     signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
@@ -230,7 +349,7 @@ pub fn deposit_in_kamino<'c, 'info>(
 }
 
 pub fn withdraw_from_kamino<'c, 'info>(
-    ctx: Context<'_, '_, 'c, 'info, Rebalance<'info>>,
+    ctx: &Context<'_, '_, 'c, 'info, Rebalance<'info>>,
     amount: u64,
     signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
